ed80ee702b8dee84059fca325882b80398f23714
Finn <piskorzm@hotmail.com>
2020-07-24 11:34:07 +0200

    Initializing MVC Folders

---
+++ ChatApp/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChatApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            if (Session["user"] != null) return Redirect("/chat");
+
+            return View();
+        }
+    }
+}
