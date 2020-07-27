8c046ee2da117d1fd6d34aca1c4352e27c026cf9
Finn <piskorzm@hotmail.com>
2020-07-27 15:13:44 +0200

    Added SessionHelper, CharController and finished AuthController.

--- ChatApp/Controllers/AuthController.cs
+++ ChatApp/Controllers/AuthController.cs
@@ -2,10 +2,26 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ChatApp.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ChatApp.Controllers
 {
-    public class AuthController
+    public class AuthController : Controller
     {
+        [HttpPost]
+        public IActionResult Login()
+        {
+            string user_name = Request.Form["username"];
+
+            if (user_name.Trim() == "")
+            {
+                return Redirect("/");
+            }
+            using (var db = new Models.ChatDbContext(options=> options.GetConnection))
+            {
+
+            }
+        }
     }
 }
--- ChatApp/Models/ChatDbContext.cs
+++ ChatApp/Models/ChatDbContext.cs
@@ -14,7 +14,7 @@ namespace ChatApp.Models
 
         }
 
-        public DbSet<User> Users { get; set; }
+        public DbSet<User> User { get; set; }
 
     }
 }
