857045ebe381ec7493100c97614d71d03ce7c330
mateuszpiskorz <piskorzm@hotmail.com>
2020-07-27 15:22:36 +0200

    Work in ChatController.

--- ChatApp/Controllers/ChatController.cs
+++ ChatApp/Controllers/ChatController.cs
@@ -3,14 +3,30 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using 
+using ChatApp.Helpers;
+using ChatApp.Models;
 
 namespace ChatApp.Controllers
 {
     public class ChatController:Controller
     {
+        private readonly ChatDbContext _context;
+        public ChatController(ChatDbContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
+            if (SessionHelper.GetObjectFromJson<User>(HttpContext.Session, "user") == null)
+            {
+                return Redirect("/");
+            }
+
+            var currentUser = SessionHelper.GetObjectFromJson<User>(HttpContext.Session, "user");
+
+            ViewBag.allUsers = _context.User.Where(u => u.Name != currentUser.Name).ToList();
+            ViewBag.currentUser = currentUser;
+            return View();
 
         }
     }
