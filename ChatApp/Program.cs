9cbc4a1d8d96d33cbb43f3f43caeab1c27254c7d
mateuszpiskorz <piskorzm@hotmail.com>
2020-08-23 12:35:32 +0200

    Finished DatabaseSeeder

--- ChatApp/Program.cs
+++ ChatApp/Program.cs
@@ -7,6 +7,10 @@ using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using ChatApp.Seeders;
+using ChatApp.Models;
 
 namespace ChatApp
 {
@@ -14,7 +18,23 @@ namespace ChatApp
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            var host = CreateWebHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    UserDbSeeder.Initialize(services);
+                }
+                catch (Exception e)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(e, "An error occured while seeding DB.");
+                }
+            }
+            host.Run();
+
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
--- ChatApp/Seeders/UserDbSeeder.cs
+++ ChatApp/Seeders/UserDbSeeder.cs
@@ -3,17 +3,62 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using ChatApp.Models;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
 
 
 namespace ChatApp.Seeders
 {
     public static class UserDbSeeder
     {
-        private ChatDbContext db = I;
 
-       
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var db = new ChatDbContext(serviceProvider.GetRequiredService<DbContextOptions<ChatDbContext>>()))
+            {
+                if (db.User.Any())
+                {
+                    return;
+                }
 
-        
-        if()
+                db.User.AddRange(
+                    new User
+                    {
+                        Name = "Harry",
+                        Password = "1212",
+                        Mail = "hpotter@hogwart.com",
+                        CreateTime = DateTime.Today
+
+                    },
+                    new User
+                    {
+                        Name = "Derek",
+                        Password = "1212",
+                        Mail = "derek@mymail.com",
+                        CreateTime = DateTime.Today
+
+                    },
+                    new User
+                    {
+                        Name = "Andrew",
+                        Password = "1212",
+                        Mail = "ajonsons@hogwart.com",
+                        CreateTime = DateTime.Today
+
+                    },
+                    new User
+                    {
+                        Name = "Harry",
+                        Password = "1212",
+                        Mail = "hpotter@hogwart.com",
+                        CreateTime = DateTime.Today
+
+                    });
+
+                db.SaveChanges();
+            }
+
+
+        }
     }
 }
